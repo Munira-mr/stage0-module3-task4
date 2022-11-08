27a1db8ffdd566634740f6ff1a1d63cfb297b644
munira-mr <munirarizaeva1@gmail.com>
2022-11-08 16:13:06 -0500

    solved...

--- src/main/java/lang/print/gaps/task4/FormulaCreator.java
+++ src/main/java/lang/print/gaps/task4/FormulaCreator.java
@@ -2,5 +2,14 @@ package lang.print.gaps.task4;
 
 public class FormulaCreator {
     public static void main(String[] args) {
+        int two=2;
+        int four=4;
+        int five=5;
+        int seven=7;
+        int nine=9;
+        char a='a';
+        char b='b';
+
+        System.out.println("("+nine+a+"-"+five+b+"+"+two+"+"+a+"-"+seven+")*("+a+"+"+b+"-"+four+a+b+"/"+two+")");
     }
 }
